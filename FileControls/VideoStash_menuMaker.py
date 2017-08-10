7d03ef0c793d74c0f1a0e0ecb5d2c0b1a9127f7b
Roger Ho <PingRogerChe1995.Ho@gmail.com>
2017-08-10 11:31:06 -0700

    Works as wanted, yet still a bug where when it hits the 'menu' folder, it stops copying and for some reason, jumps out of the loop and could not execute the lower files. Will get back to it to figure out the 'reason'. For now, just changed the 'menu' to 'zzzzzMENUzzzzz' to make sure it starts with the 'z' alphabet so it will be at the very bottom of the files so it won't trigger the bug

--- FileControls/VideoStash_menuMaker.py
+++ FileControls/VideoStash_menuMaker.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, glob
 
 
 
@@ -6,32 +6,48 @@ def makingMenu(path):
 
 	
 	os.chdir(path) # getting all the files in all the directories and subdirectories in current path
-	menu_folder = "menu"
+	menu_folder = "zzzzzMENUzzzzz" #somehow stops at the menu itself
 	if not os.path.exists(menu_folder):
 		os.mkdir(menu_folder)
 		print("making dir")
 	pictures = []
-	print(path)
-	fileTree = os.walk(path)
-	for dirname, subdir, files in fileTree:
+	#print(path)
+	dirs = os.listdir(path)
 
-		#if subdir != "[]" or subdir != menu_folder:
-		#basename = os.path.basename(dirname)
+	#print(dirs)
+	
+	for directory in dirs:
+		#print(directory)
+		files = glob.glob(path + "/" + directory + "/*.jpg")
 		print(files)
-		#print(dirname, subdir, files)
-
+		print("asdfasdfasdf123409810-91--______+++++___+++")
+		#os.chdir(directory)
+		
+		"""
+		files = os.listdir(directory)
 		for file in files:
+			#if subdir != "[]" or subdir != menu_folder:
+			#basename = os.path.basename(dirname)
+			#print(dirname, subdir, files)
+
+			print(file)
 			fileType = file.split(".")[-1]
 			#print(fileType)
 			if fileType == "jpg":
 				pictures.append(file)  #finding the pictures' names
 				#print(pictures)
-		for file in pictures:
+		"""
+	#print(pictures)
+		for file in files:
+			if file == "[]" or file == None:
+				continue
+			print("\n=====")
 			print(file)
-			destfile = path + '/' + menu_folder + "/" + file
-			srcfile = subdir + "/" + file
-			print(destfile)
-			print(srcfile)
+			print("\n =====")
+			destfile = path + '/' + menu_folder + "/" + file.split("/")[0]
+			srcfile = file
+			#print(destfile)
+			#print(srcfile)
 			'''
 			destfile = "{}.{}".format(menu_folder + "/" + file)
 			srcfile = dirname + "/" + file
