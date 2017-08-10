c2ee0049dd40efda34a533b04a9280382c95774f
Roger Ho <PingRogerChe1995.Ho@gmail.com>
2017-08-10 10:56:05 -0700

    stil trying

--- FileControls/VideoStash_menuMaker.py
+++ FileControls/VideoStash_menuMaker.py
@@ -2,34 +2,57 @@ import os, shutil
 
 
 
-def makingMenu(subdirpath):
+def makingMenu(path):
 
-	cur_path = os.path.dirname(subdirpath) # get current path
-	fileTree = os.walk(cur_path) # getting all the files in all the directories and subdirectories in current path
+	
+	os.chdir(path) # getting all the files in all the directories and subdirectories in current path
 	menu_folder = "menu"
-	os.mkdir(menu_folder)
+	if not os.path.exists(menu_folder):
+		os.mkdir(menu_folder)
+		print("making dir")
 	pictures = []
-
+	print(path)
+	fileTree = os.walk(path)
 	for dirname, subdir, files in fileTree:
-		basename = os.path.basename(dirname)
 
+		#if subdir != "[]" or subdir != menu_folder:
+		#basename = os.path.basename(dirname)
+		print(files)
+		#print(dirname, subdir, files)
 
 		for file in files:
 			fileType = file.split(".")[-1]
+			#print(fileType)
 			if fileType == "jpg":
-				pictures.append(file)  # finding the pictures and put their "pathways" into the list for further ues
-
+				pictures.append(file)  #finding the pictures' names
+				#print(pictures)
 		for file in pictures:
-			filename = file.split(".")[0]
-			destfile = "{}.{}".format(menu_folder + "/" + filename, fileType)
+			print(file)
+			destfile = path + '/' + menu_folder + "/" + file
+			srcfile = subdir + "/" + file
+			print(destfile)
+			print(srcfile)
+			'''
+			destfile = "{}.{}".format(menu_folder + "/" + file)
 			srcfile = dirname + "/" + file
+			'''
 			shutil.copy(srcfile, destfile)
+	#os.chdir(parentpath)
 
 def loopingDir():
-	cur_path = os.path.dirname(__file__)
+	cur_path = os.getcwd()
+	
 	fileTree = os.walk(cur_path)
+	#print("hello")
 	for dirname, subdir, files in fileTree:
-		#basename = os.path.basename(dirname)
-		makingMenu(subdir)
+		#print(dirname, subdir, files)
+		for dirnames in subdir:
+			path = cur_path + '/' + dirnames
+			#basename = os.path.basename(dirname)
+			try:
+				makingMenu(path)
+			except:
+				continue
 
+loopingDir()
 
