d7f2cee9fa0c25a2bf3dfd1c6acb4c9180db9abc
Roger Ho <PingRogerChe1995.Ho@gmail.com>
2017-08-16 23:38:35 -0700

    more added

--- PM2.5/pandasForPM2.5.py
+++ PM2.5/pandasForPM2.5.py
@@ -27,4 +27,18 @@ df[df.Kills >= 10]
 df.values # acquires ALL data, represented in a 2 dimensional list [[]]
 df.values[0] # here, we are getting TEETO's KDA (data)
 df.values[0][1] # getting TEETO's Deaths data value
+# the down part of using the DataFrame "values" attribute is that we needa know the EXACT INDEX of our data we want to acquire
+# so the better usage for acquiring data is the "loc" attribute
 
+df.loc["TEETO", : ] # df.loc[row_name, column_name]   use " : " to mean "ALL" data
+df.loc[("TEETO", "Trist"), : ]
+df.loc[("TEETO", "Trist"), ("Kills", "Deaths")] #more than one certain data, use '()' to put em together
+df.loc["TEETO":"Urgot", "Deaths" : "Assists"] # what to (:) what
+df.loc[ : "Urgot", "Deaths" : ]  #start to(:) what, something to(:) End
+
+#the "iloc" attribute uses (row, column)
+df.iloc[1, : ]
+df.iloc[1][1] #.iloc is the exact same usage as .loc, but it uses the exact index
+
+df.ix[]  # works exactly like the same
+ 
